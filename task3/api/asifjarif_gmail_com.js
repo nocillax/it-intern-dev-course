0d327d5c417fd99b4520b67dd65c2b80a5bdd2fa
nocillax_sp8 <asifjarif@gmail.com>
2025-09-23 17:44:12 +0600

    redid the entire code

--- task3/api/asifjarif_gmail_com.js
+++ task3/api/asifjarif_gmail_com.js
@@ -2,26 +2,30 @@ module.exports = (req, res) => {
   const xStr = req.query.x;
   const yStr = req.query.y;
 
-  // Must be non-empty digits
+  // Only non-negative integers allowed, no extra characters
   if (!/^\d+$/.test(xStr) || !/^\d+$/.test(yStr)) {
     res.status(200).send("NaN");
     return;
   }
 
-  const x = parseInt(xStr, 10);
-  const y = parseInt(yStr, 10);
+  const x = BigInt(xStr);
+  const y = BigInt(yStr);
 
-  // Must be natural numbers (>= 1)
-  if (x <= 0 || y <= 0) {
-    res.status(200).send("NaN");
+  // Special case: both zero
+  if (x === 0n && y === 0n) {
+    res.status(200).send("0");
     return;
   }
 
+  // GCD using BigInt
   const gcd = (a, b) => {
-    while (b !== 0) [a, b] = [b, a % b];
+    while (b !== 0n) {
+      [a, b] = [b, a % b];
+    }
     return a;
   };
 
+  // LCM using BigInt
   const lcm = (a, b) => (a * b) / gcd(a, b);
 
   res.status(200).send(lcm(x, y).toString());
