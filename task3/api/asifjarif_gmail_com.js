c0158b44c37ef196618d2f12e261f16ecec4ccf1
nocillax_sp8 <asifjarif@gmail.com>
2025-09-23 17:22:51 +0600

    Update asifjarif_gmail_com.js

--- task3/api/asifjarif_gmail_com.js
+++ task3/api/asifjarif_gmail_com.js
@@ -1,25 +1,27 @@
-const gcd = (x, y) => {
-  while (y !== 0) {
-    [x, y] = [y, x % y];
-  }
-  return x;
-};
-
-const lcm = (x, y) => {
-  return (x * y) / gcd(x, y);
-};
-
 module.exports = (req, res) => {
-  const xStr = parseInt(req.query.x);
-  const yStr = parseInt(req.query.y);
+  const xStr = req.query.x;
+  const yStr = req.query.y;
 
-  if (!/^[1-9]\d*$/.test(xStr) || !/^[1-9]\d*$/.test(yStr)) {
+  // Must be all digits, non-empty
+  if (!/^\d+$/.test(xStr) || !/^\d+$/.test(yStr)) {
     res.status(200).send("NaN");
     return;
   }
 
   const x = parseInt(xStr, 10);
   const y = parseInt(yStr, 10);
+
+  if (x <= 0 || y <= 0) {
+    res.status(200).send("NaN");
+    return;
+  }
+
+  const gcd = (a, b) => {
+    while (b !== 0) [a, b] = [b, a % b];
+    return a;
+  };
+
+  const lcm = (a, b) => (a * b) / gcd(a, b);
 
   res.status(200).send(lcm(x, y).toString());
 };
