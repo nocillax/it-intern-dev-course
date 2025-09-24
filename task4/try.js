a8d871a05a1ab7ceec438aff4645575b0ba62817
nocillax_sp8 <asifjarif@gmail.com>
2025-09-24 16:02:43 +0600

    Update try.js

--- task4/try.js
+++ task4/try.js
@@ -1,24 +1,28 @@
-const randomNumber = (a, b) => {
-  return Math.floor(Math.random() * b) + a;
-};
-
-const randomNum = randomNumber(1, 5);
-let userNumber = 0;
-
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("", (answer) => {
-  userNumber = parseInt(answer, 10);
+const randomNumber = (b) => {
+  return Math.floor(Math.random() * b);
+};
 
-  if (userNumber === randomNum) {
-    console.log("Congratulations! You guessed the number.");
-  } else {
-    console.log(`Sorry, the correct number was ${randomNum}.`);
-  }
-  rl.close();
+let noOfBoxes = 0;
+
+rl.question("Enter Number of Boxes.. ", (number) => {
+  noOfBoxes = parseInt(number, 10);
+  const gunInsideBox = randomNumber(noOfBoxes);
+
+  console.log("Gun is in box number: ", gunInsideBox);
+
+  rl.question("Enter Box Number to open: ", (answer) => {
+    const boxNumber = parseInt(answer, 10);
+    if (boxNumber === gunInsideBox) {
+      console.log("You are correct");
+    } else {
+      console.log("You are wrong");
+    }
+    rl.close();
+  });
 });
